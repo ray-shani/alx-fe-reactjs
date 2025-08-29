df84f8c40ee2699100ea910f1622c58f9f9be27a
Ray Shani <raysainepushani@gmail.com>
2025-08-29 09:22:59 +0300

    update: github services updates

--- github-user-search/src/services/githubService.js
+++ github-user-search/src/services/githubService.js
@@ -5,13 +5,27 @@ import axios from 'axios';
  * @param {string} username The GitHub username to search for.
  * @returns {Promise<object>} A promise that resolves with the user data.
  */
-export const fetchUserData = async (username) => {
+export const searchUsers = async (username, location, minRepos, page = 1) => {
+  let query = username.trim();
+  if (location.trim()) {
+    query += `+location:${location.trim()}`;
+  }
+  if (minRepos !== null && minRepos > 0) {
+    query += `+repos:>=${minRepos}`;
+  }
+
   try {
-    const response = await axios.get(`https://api.github.com/users/${username}`);
+    const response = await api.get('/search/users', {
+      params: {
+        q: query,
+        page,
+        per_page: 10,
+      },
+    });
     return response.data;
   } catch (error) {
     if (error.response && error.response.status === 404) {
-      throw new Error('Looks like we can\'t find the user.');
+      throw new Error('Looks like we can\'t find any users matching that criteria.');
     }
     throw new Error('An error occurred while fetching data. Please try again.');
   }
