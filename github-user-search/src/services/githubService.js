f0b47745ce0336b7dd8a94f667b7b7bb3d32a03e
Ray Shani <raysainepushani@gmail.com>
2025-08-29 09:54:51 +0300

    update: adding api configurations

--- github-user-search/src/services/githubService.js
+++ github-user-search/src/services/githubService.js
@@ -1,7 +1,15 @@
-// githubService.js
+API
 const GITHUB_API_URL = 'https://api.github.com/search/users';
 
+/**
+ * Searches for GitHub users based on username, location, and min repositories.
+ * @param {string} username - The main search query (username).
+ * @param {string} location - The location to filter by.
+ * @param {number} minRepos - The minimum number of public repositories.
+ * @returns {Promise<Array>} A promise that resolves to an array of user objects.
+ */
 export const searchUsers = async (username, location, minRepos) => {
+
   let query = username;
   if (location) {
     query += `+location:${location}`;
@@ -10,7 +18,15 @@ export const searchUsers = async (username, location, minRepos) => {
     query += `+repos:>=${minRepos}`;
   }
 
+
   const response = await fetch(`${GITHUB_API_URL}?q=${query}`);
+  
+  if (!response.ok) {
+    throw new Error('Failed to fetch user data');
+  }
+
   const data = await response.json();
+  
+ 
   return data.items;
 };
