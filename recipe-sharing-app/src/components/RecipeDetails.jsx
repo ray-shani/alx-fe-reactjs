4e2861ec1c204c2684edac18243fba8f06734e12
Ray Shani <raysainepushani@gmail.com>
2025-08-29 08:30:14 +0300

    update: updating recipe details

--- recipe-sharing-app/src/components/RecipeDetails.jsx
+++ recipe-sharing-app/src/components/RecipeDetails.jsx
@@ -1,13 +1,22 @@
 import useRecipeStore from '../store/recipeStore';
 import { useParams, Link } from 'react-router-dom';
-import EditRecipeForm from './EditRecipeForm';
-import DeleteRecipeButton from './DeleteRecipeButton';
 
 const RecipeDetails = () => {
   const { recipeId } = useParams();
-  const recipe = useRecipeStore(state =>
-    state.recipes.find(recipe => recipe.id.toString() === recipeId)
-  );
+  const recipe = useRecipeStore(state => state.recipes.find(recipe => recipe.id.toString() === recipeId));
+  const favorites = useRecipeStore(state => state.favorites);
+  const addFavorite = useRecipeStore(state => state.addFavorite);
+  const removeFavorite = useRecipeStore(state => state.removeFavorite);
+
+  const isFavorite = favorites.includes(recipe?.id);
+
+  const handleFavoriteClick = () => {
+    if (isFavorite) {
+      removeFavorite(recipe.id);
+    } else {
+      addFavorite(recipe.id);
+    }
+  };
 
   if (!recipe) {
     return <div>Recipe not found!</div>;
@@ -17,10 +26,12 @@ const RecipeDetails = () => {
     <div>
       <h1>{recipe.title}</h1>
       <p>{recipe.description}</p>
+      <button onClick={handleFavoriteClick}>
+        {isFavorite ? 'Remove from Favorites ❤️' : 'Add to Favorites 🤍'}
+      </button>
       <Link to={`/edit/${recipe.id}`}>
         <button>Edit Recipe</button>
       </Link>
-      <DeleteRecipeButton recipeId={recipe.id} />
     </div>
   );
 };
