172f65fcd591c8767ae58278bb1ac0c24e26d953
Ray Shani <raysainepushani@gmail.com>
2025-08-29 09:45:50 +0300

    initialize: tailwind css and postcss configurations

---
+++ github-user-search/tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{html,js}", // Adjust this to match your project structure
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
